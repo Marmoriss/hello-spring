18a9ea03690ee8a29a54632787e9d83b60b4a67f
Marmoriss <skawngml1106@naver.com>
2022-08-10 15:00:47 +0900

    dev 수정, 삭제 구현 완료

---
+++ src/main/java/com/kh/spring/common/typehandler/StringArrayTypeHandler.java
@@ -0,0 +1,52 @@
+package com.kh.spring.common.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+@MappedTypes(String[].class)
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class StringArrayTypeHandler extends BaseTypeHandler<String[]>{
+
+	/**
+	 * String[] -> VARCHAR
+	 * - parameter가 null인 경우에는 호출되지 않는다.
+	 */
+	// setter
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, String[] parameter, JdbcType jdbcType)
+			throws SQLException {
+		String value = String.join(",", parameter); // C, Java, 
+		ps.setString(i, value);
+	}
+	
+	/**
+	 * VARCHAR -> String[]
+	 */
+	// resultSet 관련 getter
+	@Override
+	public String[] getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		String value = rs.getString(columnName);
+		return value != null ? value.split(",") : null;
+	}
+	
+	@Override
+	public String[] getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		String value = rs.getString(columnIndex);
+		return value != null ? value.split(",") : null;
+	}
+	
+	// 프로시저 관련 getter
+	@Override
+	public String[] getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		String value = cs.getString(columnIndex);
+		return value != null ? value.split(",") : null;
+	}
+	
+}
